cda62188d9c68b77639ceb9af79611831d347c33
Ethan Lim <ethanlim@Ethans-MBP.lan>
2026-08-16 10:55:57 -0400

    Configure GitHub Pages deployment with GitHub Actions

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/weather-quake-dashboard/',
   plugins: [react()],
   server: {
     host: '0.0.0.0',
